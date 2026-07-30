c709dadc3b7dff3d181e5f033456ab5b82ef833f
fooLlshFool <SnoozeNap@outlook.com>
2026-07-30 10:19:44 -0400

    add some meethod in Ring and PolygonEventCoordinates

--- src/main/java/entity/PolygonEventCoordinates.java
+++ src/main/java/entity/PolygonEventCoordinates.java
@@ -7,6 +7,20 @@ public class PolygonEventCoordinates implements EventCoordinates{
     private List<Ring> innerRing;
     private String date;
 
+    public PolygonEventCoordinates(Ring outterRing, String date){
+        this.outterRing = outterRing;
+        this.innerRing = null;
+        this.date = date;
+    }
+
+    public PolygonEventCoordinates(Ring outterRing, List<Ring> innerRing, String date){
+        this.outterRing = outterRing;
+        this.innerRing = innerRing;
+        this.date = date;
+    }
+
+
+
     public String getCoordinates(){
         return
     }
--- src/main/java/entity/Ring.java
+++ src/main/java/entity/Ring.java
@@ -8,4 +8,12 @@ public class Ring {
     public Ring(List<PointCoordinates> pointCoordinatesList){
         this.pointCoordinatesList = pointCoordinatesList;
     }
+
+    public List<PointCoordinates> getPointCoordinatesList() {
+        return pointCoordinatesList;
+    }
+
+    public int size() {
+        return pointCoordinatesList != null ? pointCoordinatesList.size() : 0;
+    }
 }
